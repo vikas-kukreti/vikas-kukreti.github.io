15fc9499e6a76b8f79e222d23134509c72e994e9
Vikas Kukreti <vickydvlpr@gmail.com>
2020-03-31 12:08:04 +0530

    Slider JS library Added

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
 window.addEventListener('scroll', (e) => {
+    if(window.pageYOffset > window.innerHeight)
+        return;
     const header = document.querySelector('.header');
     header.style.backgroundPositionY = '' + window.pageYOffset/2 + 'px';
 });
